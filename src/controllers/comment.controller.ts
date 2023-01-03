260acff14c3d49fc782ee1ef40fb8080dd639a28
roxyzc <roxyzc12@gmail.com>
2023-01-03 21:10:16 +0700

    update

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -11,3 +11,24 @@ export const findComments = async (_req: Request, res: Response) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+export const comment = async (req: Request, res: Response): Promise<any> => {
+  const { content } = req.body.content;
+  try {
+    const findComment = await Comment.findById(req.params.id);
+    if (!findComment)
+      return res
+        .status(400)
+        .json({ success: false, message: "comment not found" });
+    const comment = await Comment.create({
+      userId: req.user.id,
+      content: content,
+    });
+    findComment?.comment.push(comment.id);
+    await (await findComment?.save()).populate("commentId");
+    res.status(200).json({ success: true, data: findComment });
+  } catch (error: any) {
+    logger.error(error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
