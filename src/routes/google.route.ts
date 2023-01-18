9cf4ad9dda840cdd54b4359b894893401ea6c3cc
roxyzc <roxyzc12@gmail.com>
2023-01-18 22:35:28 +0700

    update

--- src/routes/google.route.ts
+++ src/routes/google.route.ts
@@ -1,5 +1,7 @@
+import { NextFunction, Request, Response } from "express";
 import { Router } from "express";
 import passport from "passport";
+import { logger } from "../libraries/Logger.library";
 
 const route = Router();
 
@@ -10,10 +12,26 @@ route.get(
 
 route.get(
   "/api/auth/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/api/auth/fail",
-    successRedirect: "/api/auth/success",
-  })
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      passport.authenticate(
+        "google",
+        {
+          failureRedirect: "/api/auth/failure",
+        },
+        (error, user, _info): any => {
+          if (error) return res.status(400).json({ success: false, error });
+          req.login(user, (error) => {
+            if (error) throw new Error(error);
+            return res.status(200).json({ success: true, data: user });
+          });
+        }
+      )(req, res, next);
+    } catch (error: any) {
+      logger.error(error.message);
+      res.status(500).json({ success: false, message: error.message });
+    }
+  }
 );
 
 export default route;
